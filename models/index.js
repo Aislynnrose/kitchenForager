3d989189621ef6a54f8a0c9360045aa4d99777c5
Landon Waddell <Landon.waddell@gmail.com>
2021-08-02 19:50:20 -0600

    Created Food, Ingredients and User models.

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const User = require('./User');
+const Food = require('./Food');
+const Ingredients = require('./Ingredients');
+
+Ingredients.belongsTo(Food, {
+    foreignKey: 'food_id',
+    onDelete: 'CASCADE'
+});
+
+Food.hasMany(Ingredients, {
+    foreignKey: 'food_id'
+  });
+
+
+module.exports = { User, Food, Ingredients};
