3ab612ea5c09723c276c86969cc4673e77d0e4e7
Landon Waddell <Landon.waddell@gmail.com>
2021-08-07 14:20:17 -0600

    Added session to user routes lgoin.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -22,7 +22,13 @@ router.post("/login", async (req, res) => {
         .json({ message: "Incorrect email or password, please try again" });
       return;
     }
-    res.render("home");
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'You are now logged in!' });
+  });
+
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
