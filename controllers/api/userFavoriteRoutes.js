628a9c94c261f08f171359290d3ea9eb3b4d958b
Kevin Devlin <azwethinkweizkd@gmail.com>
2021-08-11 18:31:44 -0600

    Fixed backend code to recieve string from front end

--- controllers/api/userFavoriteRoutes.js
+++ controllers/api/userFavoriteRoutes.js
@@ -1,36 +1,33 @@
 const router = require("express").Router();
 require("dotenv").config();
 const apiKey = process.env.API_KEY;
-let byIngredient = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=${apiKey}`;
 const axios = require("axios");
 
-router.get("/personalHomepage", async function newFormHandler(event) {
-  event.preventDefault();
-  fetch(
-    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=5&ignorePantry=true&ranking=1",
-    {
-      method: "GET",
-      headers: {
-        "x-rapidapi-key": apiKey,
-        "x-rapidapi-host":
-          "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      },
-    }
-  )
+router.post("/personalHomepage", async (req, res) => {
+  console.log(req.body);
+  console.log(res);
+  axios
+    .get(
+      `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=${req.body}&number=5&ignorePantry=true&ranking=1`,
+      {
+        headers: {
+          "x-rapidapi-key": apiKey,
+          "x-rapidapi-host":
+            "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        },
+      }
+    )
     .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
-      console.log(response);
+      console.log("response", response.data);
+      if (response.ok) {
+        document.location.replace("/personalHomepage");
+      } else {
+        console.log("Failed to add dish");
+      }
     })
     .catch((err) => {
       console.error(err);
     });
-  if (response.ok) {
-    document.location.replace("/");
-  } else {
-    alert('Failed to add dish');
-  }
 });
 
 module.exports = router;
