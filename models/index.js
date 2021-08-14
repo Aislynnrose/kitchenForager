ff312d16f7ee6cfeef71cdedc2302a564187c229
Kevin Devlin <azwethinkweizkd@gmail.com>
2021-08-14 10:44:25 -0600

    Fixed recipe to user

--- models/index.js
+++ models/index.js
@@ -1,20 +1,24 @@
-const User = require('./User');
-const Food = require('./Food');
-const Ingredients = require('./Ingredients');
-const DevsFavs = require('./DevsFavs');
-const Recipes = require('./Recipes');
+const User = require("./User");
+const Food = require("./Food");
+const Ingredients = require("./Ingredients");
+const DevsFavs = require("./DevsFavs");
+const Recipes = require("./Recipes");
 
 Ingredients.belongsTo(Food, {
-  foreignKey: 'food_id',
-  onDelete: 'CASCADE'
+  foreignKey: "food_id",
+  onDelete: "CASCADE",
 });
 
 Food.hasMany(Ingredients, {
-  foreignKey: 'food_id'
+  foreignKey: "food_id",
+});
+
+User.hasMany(Recipes, {
+  foreignKey: "user_id",
 });
 
 Recipes.belongsTo(User, {
-  foreignKey: 'food_id'
-})
+  foreignKey: "user_id",
+});
 
 module.exports = { User, Food, Ingredients, DevsFavs, Recipes };
